a528e443eab9981537a428c0beccf9cda9458e00
Kaio Fontenele <mamakaiofrep@gmail.com>
2023-08-29 11:43:13 -0300

    POO

---
+++ Metodo.dart
@@ -0,0 +1,33 @@
+void main(){
+  Pessoa Pessoa1 = Pessoa();
+  Pessoa1.nome = 'Daniel';
+  Pessoa1.idade = 40;
+  print(Pessoa1.nome);
+  print(Pessoa1.idade);
+
+Pessoa1.aniversario();
+print(Pessoa1.idade);
+
+  Pessoa Pessoa2 = Pessoa();
+  Pessoa2.nome = 'Kiwada';
+  Pessoa2.casado = true;
+  print(Pessoa2.nome);
+  print(Pessoa2.idade);
+}
+
+
+
+class Pessoa{
+  String? nome;
+  int? idade; 
+  bool casado = false;
+
+
+  void aniversario(){
+    print('Parabéns ! $nome');
+    if( idade != null){
+      idade = idade! +1; 
+      
+    }
+  }
+}
