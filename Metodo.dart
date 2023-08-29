e9d46301d68808c837fc42296845c1c7aef3a445
Kaio Fontenele <mamakaiofrep@gmail.com>
2023-08-29 12:05:55 -0300

    Func in Class

--- Metodo.dart
+++ Metodo.dart
@@ -1,12 +1,14 @@
 void main(){
   Pessoa Pessoa1 = Pessoa();
-  Pessoa1.nome = 'Daniel';
+  Pessoa1.TrocarNome('Macaco');
   Pessoa1.idade = 40;
   print(Pessoa1.nome);
   print(Pessoa1.idade);
 
-Pessoa1.aniversario();
-print(Pessoa1.idade);
+  print(Pessoa1.aniversario());
+
+  Pessoa1.casar();
+  print(Pessoa1.casado);
 
   Pessoa Pessoa2 = Pessoa();
   Pessoa2.nome = 'Kiwada';
@@ -23,11 +25,20 @@ class Pessoa{
   bool casado = false;
 
 
-  void aniversario(){
+  int? aniversario(){
     print('Parabéns ! $nome');
     if( idade != null){
       idade = idade! +1; 
       
     }
+    return idade;
+  }
+  void casar(){
+    casado = true;
+  }
+
+  void TrocarNome(String n){
+    nome = n ;
+
   }
 }
